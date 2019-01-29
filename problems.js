e7f3b77cf0de8b8ed3aab51f1a245cc8b573733a
clyde5649 <clyde.bledsoe@yahoo.com>
2019-01-28 18:21:38 -0600

    work

---
+++ problems.js
@@ -0,0 +1,34 @@
+main
+
+function main() {
+
+    var UserInput=[1];
+
+    for (UserInput= i = 1; i <= 5; 1++);
+
+    let arr= UserInput();
+
+    else("UserInput" ===1);
+
+    else if (UserInput===quits);
+    return=print("all items")
+
+
+
+}
+
+
+///////////////////////////////////
+
+main
+
+function main() {
+
+    function translate
+
+
+
+
+
+
+}
